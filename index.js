01a8ae44df016a260971c059fdea6d0b18281441
Olena Ageyeva <eageeva@hotmail.com>
2018-05-16 11:49:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+let newDrivers=[]
+
+function findMatching(drivers, name){
+  newDrivers=[]
+  newDrivers=drivers.filter(function(element){return element.toLowerCase()===name.toLowerCase()})
+  return newDrivers
+}
+
+function fuzzyMatch(drivers, letter){
+  newDrivers=[]
+  newDrivers=drivers.filter(functions(element){return element.contains(letter)})
+  return newDrivers
+}
